fec2b43a5e6b24f4508e2791905f78bd393b3ad7
Marco Ciampini <marco.ciampo@gmail.com>
2024-09-28 21:56:42 +0200

    Composite: make items tabbable when the active element is disconnected

--- packages/components/src/composite/item.tsx
+++ packages/components/src/composite/item.tsx
@@ -20,9 +20,20 @@ export const CompositeItem = forwardRef<
 	WordPressComponentProps< CompositeItemProps, 'button', false >
 >( function CompositeItem( props, ref ) {
 	const context = useCompositeContext();
+	const store = context.store as Ariakit.CompositeStore;
+
+	// If the active item is not connected, Composite may end up in a state
+	// where none of the items are tabbable. In this case, we force all items to
+	// be tabbable, so that as soon as an item received focus, it becomes active
+	// and Composite goes back to working as expected.
+	const tabbable = Ariakit.useStoreState( store, ( state ) => {
+		return ! store?.item( state?.activeId )?.element?.isConnected;
+	} );
+
 	return (
 		<Ariakit.CompositeItem
-			store={ context.store as Ariakit.CompositeStore }
+			store={ store }
+			tabbable={ tabbable }
 			{ ...props }
 			ref={ ref }
 		/>
